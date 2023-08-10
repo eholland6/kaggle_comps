93f90a979d9ff6f6ea2188bfb599feec058ecfff
Eric Holland <hollanderic@yahoo.com>
2023-08-09 20:53:02 -0400

    Initial commit of hist_creator function

--- data_de_gama.py
+++ data_de_gama.py
@@ -1,9 +1,26 @@
+import math
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
 class DataReview:
     def __init__(self, features, target):
-        pass
+        self.features = features
+        self.target = target
+        self.num_feature_cols = self.features.shape[1]
 
-    def hist_creator(self):
-        pass
+    def hist_creator(self, save_file=False):
+        num_df = self.features.select_dtypes(np.number)
+        shape = num_df.shape
+        runs = int(math.ceil(shape[1]/9.0))
+
+        for runs in range(runs):
+            num_df.iloc[:, (0 + 9*runs):(9 + 9*runs)].hist(bins=50, figsize=(30,22))
+            if save_file:
+                plt.savefig(f'hist_img_{runs}.jpg')
+            else:
+                plt.show()
+
 
     def corr_matrix(self):
         pass
